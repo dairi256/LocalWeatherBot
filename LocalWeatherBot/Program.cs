99150f1e670070691495b381cb7053fe60533070
dairi256 <72542198+dairi256@users.noreply.github.com>
2025-12-04 19:59:45 +0800

    Add new data into 'MLNET_Weather_Training.csv'.
    For now, the best approach is to add new data by hand. Also added comments in 'Program.cs' to help users to make the raw data into the lag-shifted data. a rawData.csv file was also added for ease of use of adding new data.

--- LocalWeatherBot/Program.cs
+++ LocalWeatherBot/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-
+            // Keep this code if you want to regenerate the training data from raw data
+            // string inputFile = "rawData.csv";
+            // string outputFile = "MLNET_Weather_Training.csv";
+            // LagShiftLogic.CreateMLNETTrainingData(inputFile, outputFile);
+            // The csv file that you use for training saves in the 'bin/Debug/net10.0' folder of the project.
         }
     }
 }
